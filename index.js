ebb20b44c515b3131c665e0f164b1066abafc058
John Boker <jboker@schoolpointe.com>
2015-04-20 14:47:16 -0400

    uptime-statsd plugin

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Statsd plugin
+ *
+ * Logs ping stats to statsd
+ *
+ */
+ 
+var Ping = require('../../models/ping');
+var CheckEvent = require('../../models/checkEvent');
+var SDC = require('statsd-client');
+
+exports.initMonitor = function(options) {
+	registerStatsdLogger(options);
+};
+
+var registerStatsdLogger = function(options, sdc) {
+	
+	var config = options.config.statsd;
+	
+	var sdc = new SDC({
+		host : config.host,
+		port : config.port,
+		prefix : config.prefix
+	});
+
+	Ping.on('afterInsert', function(ping) {
+		ping.findCheck(function(err, check) {
+			var name = check.name + '';
+			name = name.replace('.', '_').replace(' ', '_').toLowerCase();
+			sdc.gauge('uptime.' + name + '.responsetime', ping.time);
+			console.log('uptime.' + name + '.responsetime: ' + ping.time);
+		});
+	});
+	
+};
